f761ad32e5c225c5d2d002fe20df9f8b6966ad18
Emerson <emerson98vera@hotmail.com>
2020-02-16 14:17:32 -0500

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -21,11 +21,11 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 */
 
 let urlDB;
-
 if(process.env.NODE_ENV === 'dev'){
     urlDB = "mongodb://localhost:27017/cafe";
 }else{
-    urlDB = "mongodb+srv://strider:7ZgEoXit9ZMDEdLy@cluster0-nrc0o.mongodb.net/cafe";
+    urlDB = process.env.MONGO_URI;
+    ;
 }
 
 process.env.URLDB =  urlDB;
